9ca73ee6f0b9a9e4a2f2cdf365daa50b6a8db130
Celeste Macías Escobar <46455194+FatyMacias@users.noreply.github.com>
2020-09-21 18:40:31 -0500

    se agrego la vista para regiones

---
+++ regiones.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <title>INDICADORES</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Rgoogle -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <!-- CSS personalizado --> 
+    <link rel="stylesheet" href="main.css">  
+      
+      
+    <!--datables CSS básico-->
+    <link rel="stylesheet" type="text/css" href="datatables/datatables.min.css"/>
+    <!--datables estilo bootstrap 4 CSS-->  
+    <link rel="stylesheet"  type="text/css" href="datatables/DataTables-1.10.18/css/dataTables.bootstrap4.min.css">
+     <!-- de aqui para abajo no se que pdo --> 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script> 
+  </head>
+  
+  <body>
+    
+    <div class="wrapper d-flex align-items-stretch">
+      <nav id="sidebar">
+      <center><a class="navbar-brand">INICIO</a></center>  
+        <div class="p-4 pt-5">
+          <a href="inicio.php" class="img logo thumbnailmb-5" style="background-image: url(images/zac.png);"></a>
+          <br>
+          <br>
+          <ul class="list-unstyled components mb-5">
+            <li class="active">
+            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">INFORMACIÓN FINANCIERA</a>
+              <ul class="collapse list-unstyled" id="homeSubmenu">
+                <li>
+                    <a href="inicio.php" onclick="openMenu('general')">Gráfica Global</a>
+                </li>
+                <li>
+                    <a href="grafica_concepto.php" onclick="openMenu('conceptos')">Gráfica Por Concepto</a>
+                </li>
+                <!-- <li>
+                    <a href="#">Por banco</a>
+                </li> -->
+                <li>
+                    <a href="grafica_subsistema.php" onclick="openMenu('genero')">Subsistemas</a>
+                </li>
+                <!-- <li>
+                    <a href="grafica_porgenero.php" onclick="openMenu('genero')">Por género</a>
+                </li> -->
+              </ul>
+            </li>
+
+
+           
+            <li>
+              <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">INFORMACIÓN DE PERSONAL</a>
+              <ul class="collapse list-unstyled" id="pageSubmenu">
+                <li>
+                    <a href="grafica_genero.php" onclick="openMenu('subsis')">Gráfica Por Género</a>
+                </li>
+                <!-- <li>
+                    <a href="#">Page 2</a>
+                </li>
+                <li>
+                    <a href="#">Page 3</a>
+                </li> -->
+              </ul>
+            </li>
+          </ul>
+       </div>
+      </nav>
+      
+      
+
+      
+
+        <!-- Page Content  -->
+        <div id="content" class="p-4 p-md-5">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <div class="container-fluid">
+            <!-- Modal -->
+   <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+        <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+        <!-- <h5 class="modal-title w-100 text-center" id="exampleModalLabel">Modal title</h5> -->
+          <h4 class="modal-title w-100 text-center" class="modal-title">INFORMACIÓN</h4>
+        </div>
+        <div class="modal-body" id="body">
+        </div>
+         <div class="modal-footer">
+          <!-- <button type="button" class="btn btn-default" data-dismiss="modal">Close</button> -->
+        </div> 
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary">Cerrar</button>
+        <!-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button> -->
+      </div>
+    </div>
+  </div>
+
+            <button type="button" id="sidebarCollapse" class="btn btn-primary">
+              <i class="fa fa-bars"></i>
+              <span class="sr-only">Toggle Menu</span>
+            </button>
+            <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fa fa-bars"></i>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="nav navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="logout.php">Cerrar sesión</a>
+                </li>
+               </ul>
+            </div>
+          </div>
+        </nav>
+
+
+
+
+        <div id="conceptos" class="w3-container menu" style="display">
+          <center><h1>INDICADORES POR REGIONES</h1></center>
+
+          <div>
+
+            <br>
+            <br>
+
+        <div class="panel-body">
+           <div class="table-responsive">
+                      <table id = "example2" class="table table-hover table-bordered" style="width:100%; border: 1px solid #ddd !important;">
+                          
+                                <thead class="thead-dark">
+                                      <tr>
+                                        <!-- <th scope="col">id</th> -->
+                                        <th scope="col">Regiones</th>
+                                        <th scope="col">Total</th>
+                                        <th scope="col">Docentes</th>
+                                        <th scope="col">Administrativos</th>
+                                        
+                                      </tr>
+                              </thead>
+                              <tbody id="colsubsis">
+
+                              </tbody>
+                              <tfoot class="thead-dark">
+                                      <tr>
+                                        <!-- <th scope="col">id</th> -->
+                                        <th scope="col">Regiones</th>
+                                        <th scope="col">Total</th>
+                                        <th scope="col">Docentes</th>
+                                        <th scope="col">Administrativos</th>
+                                        
+                                      </tr>
+
+                              </tfoot>
+                      </table>
+              </div>
+          </div>
+          </div>
+
+                
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>    
+    <!-- jQuery, Popper.js, Bootstrap JS -->
+    <script src="jquery/jquery-3.3.1.min.js"></script>
+    <script src="popper/popper.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+        <!-- datatables JS -->
+    <script type="text/javascript" src="datatables/datatables.min.js"></script>    
+     
+    <!--<script type="text/javascript" src="main.js"></script>-->
+    
+  </body>
+</html>
+
+<!-- libreria de google con internet <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script> -->
+<!-- libreria de google sin internet-->
+<script type="text/javascript" src="./charts/loader.js"></script>
+<script type="text/javascript">
