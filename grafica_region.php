0807441e49b80a7e998277e3fb1387a4ab11a395
Celeste Macías Escobar <46455194+FatyMacias@users.noreply.github.com>
2020-09-22 00:05:14 -0500

    arreglao

--- grafica_region.php
+++ grafica_region.php
@@ -37,7 +37,8 @@ $resultM = $statementM->fetchAll();
     <link rel="stylesheet" href="css/style.css">
 
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script> 
-    
+      <!-- importacion css para el toast-->
+      <link href="css/toastr.min.css" rel="stylesheet"/>
   </head>
   <body>
     
@@ -352,6 +353,8 @@ $resultM = $statementM->fetchAll();
     <script src="js/main.js"></script>
           <!-- datatables JS -->
     <script type="text/javascript" src="datatables/datatables.min.js"></script> 
+    <script src="js/toastr.min.js"></script>
+
   </body>
 </html>
 
@@ -374,16 +377,16 @@ function load_subsis(id, idd)
         method:"POST",
         data:{id:id, idd:idd},
         dataType:"JSON",
-        success:function(data)
-        {
-            drawSubsis(data);
-            drawSubsis2(data); 
-        },
-        error: function(data)
-        {
-            alert("No hay Datos");
-        }
-    });
+        success: function (data) {
+          
+          drawSubsis(data);
+          drawSubsis2(data); 
+          toastr.success('Datos cargados', '', {timeOut: 2000});
+      },
+  error: function (data) {
+          toastr.error('No se encontraron datos', 'Error', {timeOut: 2000});
+      }
+  });
 }
 
 function load_regtot(id, idd)
@@ -401,7 +404,7 @@ function load_regtot(id, idd)
         },
         error: function(data)
         {
-            alert("No hay Datos");
+            //alert("No hay Datos");
         }
     });
 }
@@ -420,7 +423,7 @@ function load_total(id, idd)
         },
         error: function(data)
         {
-            alert("No hay Datos");
+           // alert("No hay Datos");
         }
     });
 }
@@ -439,7 +442,7 @@ function load_total2(id, idd)
         },
         error: function(data)
         {
-            alert("No hay Datos");
+//alert("No hay Datos");
         }
     });
 }
