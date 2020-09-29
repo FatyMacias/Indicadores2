dd143d6ffef18adc4b5d4b67b22e72f8d4d96baf
JorgeLTC <30934020+JorgeLTC@users.noreply.github.com>
2020-09-29 14:06:15 -0500

    masomaso

--- regiones.php
+++ regiones.php
@@ -208,6 +208,7 @@ $resultM = $statementM->fetchAll();
         </table>
         <div>
         <input id = "btns" type="button" class="float-md-left btn btn-success" onclick="mostb()" value="Confirmar Seleccion">
+        <input id = "btnd" type="button" class="float-md-left btn btn-success" onclick="disable()" value="Reset">
         </div>
         <table id="example" class="table table-hover table-bordered" style="width:100%; border: 1px solid #ddd !important;">
           <thead class="thead-dark">
@@ -219,7 +220,8 @@ $resultM = $statementM->fetchAll();
               <th scope="col">Importe/Administrativos</th>
             </tr>
           </thead>
-          <tbody id="tableModify"></tbody>
+          <tbody id="tableModify">
+          </tbody>
           <tfoot class="thead-dark">
           <tr>
               <!-- <th scope="col">id</th> -->
@@ -342,7 +344,7 @@ function drawSubsis(chart_data,success)
         tablaData += '<td >'+region+'</td>';
         tablaData += '<td>'+'$'+total.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")+'</td>';
         tablaData += '<td>'+'$'+docente.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")+'</td>';
-        tablaData += '<td>'+'$'+admvos.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")+'<input id = "btn'+id+'" name = "btn" type="button" class="float-md-right btn btn-success" onclick="cloneRow('+id+')" value="Seleccionar"></td>';
+        tablaData += '<td>'+'$'+admvos.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")+'<input type="button" class=" boton float-md-right btn btn-success" onclick="cloneRow('+id+')" value="Seleccionar"></td>';
         //tablaData += '<td> <input type="button" class="btn btn-success" value="Seleccionar"> </td>'
         tablaData += '</tr>';
         
@@ -455,6 +457,11 @@ function cloneRow(region) {
      
     }
 
+function disable(){
+
+
+}
+
 function mostb(){
   $(document).ready(function() {    
     var table2 = $('#example').DataTable({  
@@ -498,9 +505,10 @@ function mostb(){
 
     ]	        
     }); 
-    $('#id, #idd').change(function(){
+
+        $("#btnd").click(function(){
           table2.clear().destroy();
-          $("#btns").prop("disabled", false);
+          //$("#btns").prop("disabled", true);
         });
 
 });
@@ -517,7 +525,8 @@ function mostb(){
 <script>
     // Detectar seleccion del select option
 $(document).ready(function(){
-
+    $("#btns").prop("disabled", true);
+    $("#btnd").prop("disabled", true);
     $('#id, #idd').change(function(){
         var id =$('#id').val();
         var idd = $('#idd').val();
@@ -525,6 +534,7 @@ $(document).ready(function(){
         {
             //alert("The text has been changed.");
             load_subsis(id, idd);
+            $("#btns").prop("disabled", false);
 
         }
     });
@@ -535,21 +545,18 @@ $(document).ready(function(){
 
   $("#btns").on("click", function() {
       $(this).prop("disabled", true);
-      $("#btn0").prop("disabled", true);
-      $("#btn1").prop("disabled", true);
-      $("#btn2").prop("disabled", true);
-      $("#btn3").prop("disabled", true);
-      $("#btn4").prop("disabled", true);
-      $("#btn5").prop("disabled", true);
-      $("#btn6").prop("disabled", true);
-      $("#btn7").prop("disabled", true);
-      $("#btn8").prop("disabled", true);
-      $("#btn9").prop("disabled", true);
-      $("#btn10").prop("disabled", true);
-      $("#btn11").prop("disabled", true);
-      $("#btn12").prop("disabled", true);
-      $("#btn13").prop("disabled", true);
+      $("#btnd").prop("disabled", false);
+      //$(".boton").prop("disabled", true);
+      $(".boton").hide();
   });
+  $("#btnd").on("click", function() {
+      $(this).prop("disabled", true);
+      $("#btns").prop("disabled", false);
+      //$(".boton").prop("disabled", true);
+      $(".boton").show();
+
+  });
+
 
 });
 </script>
