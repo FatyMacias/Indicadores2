4dfae93776901744fc420bf9ea7cb21fc567e0df
JorgeLTC <30934020+JorgeLTC@users.noreply.github.com>
2020-09-29 00:21:50 -0500

    pdf

--- regiones.php
+++ regiones.php
@@ -365,45 +365,45 @@ function drawSubsis(chart_data,success)
     var chart = new google.visualization.ColumnChart(document.getElementById('chart_area'));
     chart.draw(data, options);
 
-    $(document).ready(function () {
-      var table = $("#example2").DataTable({
-      bSort : false,
-      order: [],
-      lengthMenu: [
-        [10, 25, 50, 100, 200, -1],
-        [10, 25, 50, 100, 200, "All"],
-      ],
-      language: {
-        lengthMenu: "Mostrar _MENU_ registros",
-        zeroRecords: "No se encontraron resultados",
-        info:
-          "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-        infoEmpty: "Mostrando registros del 0 al 0 de un total de 0 registros",
-        infoFiltered: "(filtrado de un total de _MAX_ registros)",
-        sSearch: "Buscar:",
-        oPaginate: {
-          sFirst: "Primero",
-          sLast: "Último",
-          sNext: "Siguiente",
-          sPrevious: "Anterior",
-        },
-        sProcessing: "Procesando...",
-      },
-      //para usar los botones
-      responsive: "true",
-      dom: "Bfrtilp",
-      buttons: [
-        {
+    $(document).ready(function() {    
+    var table = $('#example2').DataTable({        
+        language: {
+                "lengthMenu": "Mostrar _MENU_ registros",
+                "zeroRecords": "No se encontraron resultados",
+                "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+                "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+                "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+                "sSearch": "Buscar:",
+                "oPaginate": {
+                    "sFirst": "Primero",
+                    "sLast":"Último",
+                    "sNext":"Siguiente",
+                    "sPrevious": "Anterior"
+			     },
+			     "sProcessing":"Procesando...",
+            },
+        //para usar los botones   
+        responsive: "true",
+        dom: 'Bfrtilp',       
+        buttons:[ 
+			{
 				extend:    'excelHtml5',
 				text:      '<i class="fas fa-file-excel"></i> ',
 				titleAttr: 'Exportar a Excel',
 				className: 'btn btn-success'
-			}
-      ],
-    });
-      $('#id, #idd').change(function(){
-      table.clear().destroy();
-    });
+			},
+			{
+				extend:    'pdfHtml5',
+				text:      '<i class="fas fa-file-pdf"></i> ',
+				titleAttr: 'Exportar a PDF',
+				className: 'btn btn-danger'
+			},
+
+		]	        
+    });  
+    $('#id, #idd').change(function(){
+          table.clear().destroy();
+        });   
 });
 
 
